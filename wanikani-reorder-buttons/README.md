d6e739c30a0186d537df2920525a635abf39cecb
Dan Herrera <sonarius@shew.io>
2021-11-24 05:57:52 -0500

    Rewrite and revamp reorder-button script

--- wanikani-reorder-buttons/README.md
+++ wanikani-reorder-buttons/README.md
@@ -7,33 +7,57 @@ This script started is a modified copy of Towe's
 [WaniKani SRS Reorder Button](https://greasyfork.org/en/scripts/29673-wanikani-srs-reorder-button).
 Their original WaniKani community post can be found
 [here](https://community.wanikani.com/t/wanikani-srs-reorder-button/17880). Most
-of the credit for this script goes to them, and serious thanks for publishing it
-too!
+of the credit for this script goes to them! Major thanks as well to rfindley and
+BIsTheAnswer for their help in the forums, as well as rfindley's publishing of
+the [WaniKani Open
+Framework](https://github.com/rfindley/wanikani-open-framework)!
 
 ## Overview
 
-![UI](ui.png)
+![Buttons](ordering-buttons.png)
 
-This script will add two buttons to the bottom of the WaniKani review page that
-will reorder your remaining reviews by their SRS order.
+This script will add two buttons inside a context menu on the top right of your
+reviews page.
 
-The ordering can be either ascending or descending, though on start-up, items
-will be reordered in ascending order. Review items will also have their meaning
-and readings be asked in immediate succession.
+The ordering can be either ascending or descending, though this plugin can be
+configured to automatically sort items in ascending order at start up. It is
+also possible to to make review items have their meaning and readings be asked
+in immediate succession as well.
+
+![Counters](review-counters.png)
 
 There is also an indicator added to the top of the page indicating the amount of
-remaining reviews for each SRS level.
+remaining reviews for each SRS level. These will update as you answer your
+review questions.
+
+## Persistent Settings
+
+Leveraging the WaniKani Open Framework, this script integrates into the common
+menu panel like other scripts. A new menu item will be installed that opens the
+following settings panel for this plugin:
+
+![Settings](settings.png)
+
+From here you can customize hotkeys and sorting behavior in a persistent manner.
 
 ## HotKeys
 
-There are two hotkeys registered for this script:
+There are two hotkeys registered by default for this script:
 
 - `Alt` + `Shift` + `-`: reorder review items by SRS level in descending order
 - `Alt` + `Shift` + `=`: reorder review items by SRS level in ascending order
 
+These are entirely customizable and toggleable within the
+[settings](#persistent-settings) panel!
+
 # Install
 
-Click
+Before installing this script, you'll need both a UserScript engine as well as
+the WaniKani Open Framework user ([click here to install
+it](https://greasyfork.org/en/scripts/38582-wanikani-open-framework)) script
+pre-installed.
+
+Once those prerequisites are met, you can click
 [here](https://github.com/loksonarius/wanikani-userscripts/raw/master/wanikani-reorder-buttons/script.user.js)
 to have your UserScript engine prompt you for installation. This has been tested
 to work with the [Tampermonkey](https://tampermonkey.net) userscript engine.
--- wanikani-reorder-buttons/script.user.js
+++ wanikani-reorder-buttons/script.user.js
@@ -4,7 +4,7 @@
 // @homepage    https://github.com/loksonarius/wanikani-userscripts/tree/master/wanikani-reorder-buttons
 // @author      Sonarius
 // @description Adds button enabling item ordering by SRS level.
-// @version     1.0.0
+// @version     2.0.0
 // @grant       none
 //
 // @include     *://www.wanikani.com/review/session
@@ -13,101 +13,317 @@
 // @downloadURL https://github.com/loksonarius/wanikani-userscripts/raw/master/wanikani-reorder-buttons/script.user.js
 // ==/UserScript==
 
-/* Settings */
+/* Dependency Checking */
+const wkof_version_needed = '1.0.51';
+if (!window.wkof) {
+  alert('[WaniKani Reorder Buttons] requires Wanikani Open Framework.\nYou will now be forwarded to installation instructions.');
+  window.location.href = 'https://community.wanikani.com/t/instructions-installing-wanikani-open-framework/28549';
+  return;
+}
 
-const questionTypeOrder = 1; // 1 - reading first, 2 - meaning first, 3 - random
-const itemTypeOrder = 1;     // 1 - rad->kan->voc, 2 - voc->kan->rad, 3 - random
-const ascendingSRS = true;   // low-level items first
-const priotitizeSRS = true;  // SRS order more important than item type order
-const force1x1 = true;       // meaning and reading directly next to each other
-const sortOnStartup = true;  // sort items on startup
-const ascHotKey = 'Equal'    // keyboard shortcut (key code)
-const desHotKey = 'Minus'    // keyboard shortcut (key code)
+const wkof = window.wkof
+if (!wkof.version || wkof.version.compare_to(wkof_version_needed) === 'older') {
+  alert('[WaniKani Reorder Buttons] requires Wanikani Open Framework version '+wkof_version_needed+'.\nYou will now be forwarded to update page.');
+  window.location.href = 'https://greasyfork.org/en/scripts/38582-wanikani-open-framework';
+  return;
+}
 
-/* Utilities */
-
+/* Constants */
 const $ = window.$;
+const jstor = $.jStorage;
+const script_settings_id = 'sonarius_wk_reorderbuttons';
 
-function getTypePriority(item) {
-  if (item.rad) {
-    return 1;
-  } else if (item.kan) {
-    return 2;
-  } else {
-    return 3;
-  }
+/* Util */
+async function fetch_review_items(subject_ids) {
+  // ideally, we'd only rely on documented endpoints, but there doesn't seem to
+  // be any documented item in the API matching what's returned by this endpoint
+  const subject_ids_str = subject_ids.join(',');
+  return await fetch(`/review/items?ids=${subject_ids_str}`)
+    .then(function(resp) {
+      return resp.json();
+    });
 }
 
-function ascComparator(itemA, itemB) {
-  const srsOrder = ascendingSRS ? itemA.srs - itemB.srs : itemB.srs - itemA.srs;
-  const typeOrder = itemTypeOrder === 3 ? 0 : (getTypePriority(itemA) - getTypePriority(itemB)) * (3 - itemTypeOrder * 2);
-  return priotitizeSRS ? srsOrder || typeOrder : typeOrder || srsOrder;
+function log(s) {
+  console.log(`[Reorder Buttons] ${s}`);
 }
 
-function desComparator(itemA, itemB) {
-  const srsOrder = ascendingSRS ? itemB.srs - itemA.srs : itemA.srs - itemB.srs;
-  const typeOrder = itemTypeOrder === 3 ? 0 : (getTypePriority(itemA) - getTypePriority(itemB)) * (3 - itemTypeOrder * 2);
-  return priotitizeSRS ? srsOrder || typeOrder : typeOrder || srsOrder;
+/* Lookup Data */
+let items_by_id = {};
+async function load_assignments() {
+  log('prepping lookup data...');
+  const config = { wk_items: { filters: { srs: {
+    invert: true, value: 'lock, init, burn'
+  }}}};
+
+  await wkof.ItemData.get_items('assignments', config)
+    .then(function(items) {
+      items_by_id = wkof.ItemData.get_index(items, 'subject_id');
+    });
 }
 
-function showCounters(items) {
-  const itemsByLevels = [0, 0, 0, 0, 0, 0, 0, 0];
-  for (let i = 0; i < items.length; ++i) {
-    ++itemsByLevels[items[i].srs - 1];
-  }
-  const $srsCounters = $('<div id="srsCounters" style="background-color:rgba(255,255,255,0.9);border-radius:8px;color:black;font-weight:bold;margin-top:5px;text-shadow:none"></div>');
-  for (let level = 1; level <= itemsByLevels.length; ++level) {
-    const color = level < 5 ? 'DD0093' : level < 7 ? '882D9E' : level < 8 ? '294DDB' : '0093DD';
-    if (level > 1) {
-      $srsCounters.append(', ');
+/* Settings */
+function install_menu() {
+  log('installing menu...');
+  wkof.Menu.insert_script_link({
+    name: script_settings_id+'_settings',
+    submenu: 'Settings',
+    title: 'Reorder Buttons',
+    on_click: open_settings
+  });
+  wkof.Menu.insert_script_link({
+    name: script_settings_id+'_sort_asc',
+    submenu: 'Sort Reviews',
+    title: 'Ascending',
+    on_click: function() { sort_queue(); }
+  });
+  wkof.Menu.insert_script_link({
+    name: script_settings_id+'_sort_des',
+    submenu: 'Sort Reviews',
+    title: 'Descending',
+    on_click: function() { sort_queue(false); }
+  });
+}
+
+function load_settings() {
+  log('loading settings...');
+  const defaults = {
+    sort_on_startup: false,
+    item_type_order: 'rkv',
+    prioritize_srs: true,
+    force1x1: false,
+    ascending_key: 'Equal',
+    ascending_modifiers: {
+      ctrl: false,
+      alt: true,
+      shift: true
+    },
+    descending_key: 'Minus',
+    descending_modifiers: {
+      ctrl: false,
+      alt: true,
+      shift: true
+    },
+  };
+  return wkof.Settings.load(script_settings_id, defaults);
+}
+
+function open_settings() {
+  const config = {
+    script_id: script_settings_id,
+    title: 'Settings',
+    on_save: update_settings,
+    content: {
+      sorting_group: {
+        type: 'group',
+        label: 'Sorting Behavior',
+        content: {
+          sort_on_startup: {
+            type: 'checkbox',
+            label: 'Sort on Startup',
+            hover_tip: 'Enables auto-sorting of reviews upon loading.'
+          },
+          item_type_order: {
+            type: 'dropdown',
+            label: 'Item-Type Ordering',
+            hover_tip: 'Determines ordering between radicals, kanji, and vocabulary.',
+            content: {
+              rkv: "Radical -> Kanji -> Vocabulary",
+              vkr: "Vocabulary -> Kanji -> Radical",
+              ran: "Random"
+            }
+          },
+          prioritize_srs: {
+            type: 'checkbox',
+            label: 'Prioritize SRS',
+            hover_tip: 'Sort by SRS level before sorting by item type.'
+          },
+          force1x1: {
+            type: 'checkbox',
+            label: 'Force 1x1',
+            hover_tip: 'Force meaning and reading questions to be in succession.'
+          },
+        }
+      },
+      keycombo_group: {
+        type: 'group',
+        label: 'Hotkeys',
+        content: {
+          ascending_group: {
+            type: 'group',
+            label: 'Ascending Sort Hotkeys',
+            content: {
+              ascending_key: {
+                type: 'text',
+                label: 'Activation Key Event Code',
+                hover_tip: 'Press this key to sort reviews in ascending order.'
+              },
+              ascending_helptext: {
+                type: 'html',
+                label: 'Note',
+                html: '<i>Use <a href="https://keycode.info" target="_blank">this site</a> to get the <em>event.code</em> for your desired binding</i>',
+              },
+              ascending_modifiers: {
+                type: 'list',
+                label: 'Modifier Keys',
+                hover_tip: 'Modifier keys that must be pressed along activation key.',
+                multi: true,
+                content: {
+                  ctrl: "Control",
+                  alt: "Alt",
+                  shift: "Shift",
+                }
+              },
+            }
+          },
+          descending_group: {
+            type: 'group',
+            label: 'Descending Sort Hotkey',
+            content: {
+              descending_key: {
+                type: 'text',
+                label: 'Activation Key Event Code',
+                hover_tip: 'Press this key to sort reviews in descending order.'
+              },
+              descending_helptext: {
+                type: 'html',
+                label: 'Note',
+                html: '<i>Use <a href="https://keycode.info" target="_blank">this site</a> to get the <em>event.code</em> for your desired binding</i>',
+              },
+              descending_modifiers: {
+                type: 'list',
+                label: 'Modifier Keys',
+                hover_tip: 'Modifier keys that must be pressed along activation key.',
+                multi: true,
+                content: {
+                  ctrl: "Control",
+                  alt: "Alt",
+                  shift: "Shift",
+                }
+              },
+            }
+          }
+        }
+      }
     }
-    $srsCounters.append($('<span style="color:#' + color + ';margin:0">' + itemsByLevels[level - 1] + '</span>'));
   }
-  $('#srsCounters').remove();
-  $('div#stats').append($srsCounters);
+  const dialog = new wkof.Settings(config);
+  dialog.open();
 }
 
-/* Event handlers */
-
-const usedUIDs = [];
-
-function reorderQuestionTypes() {
-  const item = $.jStorage.get('currentItem');
-  const newUID = (item.rad ? 'r' : item.kan ? 'k' : 'v') + item.id;
-  if (usedUIDs.includes(newUID)) {
-    return;
-  }
-  usedUIDs.push(newUID);
-  const requestedType = ['reading', 'meaning'][item.rad ? 1 : questionTypeOrder - 1];
-  if ($.jStorage.get("questionType") !== requestedType) {
-    $.jStorage.set('questionType', requestedType);
-    $.jStorage.set('currentItem', item);
-  }
+function update_settings() {
+  log('settings saved!');
 }
 
-function updateCounters() {
-  const items = $.jStorage.get('activeQueue').concat($.jStorage.get('reviewQueue'));
-  showCounters(items);
-  return items;
+/* Hotkeys */
+function register_hotkeys() {
+  document.addEventListener('keydown', function(e) {
+    if (event.defaultPrevented) {
+      return;
+    }
+
+    const settings = wkof.settings[script_settings_id]
+    const asc_key = settings.ascending_key
+    const asc_mod = settings.ascending_modifiers
+    const des_key = settings.descending_key
+    const des_mod = settings.descending_modifiers
+
+    switch (e.code) {
+      case asc_key:
+        if (e.shiftKey == asc_mod.shift &&
+          e.altKey == asc_mod.alt &&
+          e.ctrlKey == asc_mod.ctrl) {
+          sort_queue();
+        }
+        break;
+      case des_key:
+        if (e.shiftKey == des_mod.shift &&
+          e.altKey == des_mod.alt &&
+          e.ctrlKey == des_mod.ctrl) {
+          sort_queue(false);
+        }
+        break;
+      default:
+        return;
+    }
+
+    e.preventDefault();
+  });
 }
 
-function reorderBySrs(ascending = true) {
-  const items = updateCounters();
-  if (ascending) {
-      items.sort(ascComparator);
-  } else {
-      items.sort(desComparator);
+/* Sorting */
+function type_priority_of(id) {
+  const val = items_by_id[`${id}`]
+  if (!val) {
+    return -1;
   }
 
-  $.jStorage.set('activeQueue', items.slice(0, 10));
-  $.jStorage.set('reviewQueue', items.slice(10).reverse());
-  if (questionTypeOrder !== 3) {
-    $.jStorage.listenKeyChange('currentItem', reorderQuestionTypes);
+  switch (val.assignments.subject_type) {
+    case 'radical':
+      return 1;
+    case 'kanji':
+      return 2;
+    case 'vocabulary':
+      return 3;
   }
-  $.jStorage.listenKeyChange('currentItem', updateCounters);
-  $.jStorage.set('currentItem', items[0]);
 
-  if (force1x1) {
+  return -1;
+}
+
+function srs_stage_for(id) {
+  const val = items_by_id[`${id}`]
+  return !val ? -1 : val.assignments.srs_stage;
+}
+
+function make_comparator(ascending=true) {
+  const srs = function(a,b) {
+    const res = srs_stage_for(a) - srs_stage_for(b);
+    if (!ascending) {
+      return -res;
+    }
+
+    return res;
+  };
+
+  const settings = wkof.settings[script_settings_id];
+  const type_order = settings.item_type_order;
+  const type = function(a,b) {
+    const res = type_priority_of(a) - type_priority_of(b);
+    switch (type_order) {
+      case 'rkv':
+        return res;
+      case 'vkr':
+        return -res;
+      default:
+        return 0;
+    }
+  };
+
+  if (settings.prioritize_srs) {
+    return function(a,b) { return srs(a,b) || type(a,b); };
+  }
+
+  return function(a,b) { return type(a,b) || srs(a,b); };
+}
+
+async function sort_queue(ascending=true) {
+  log('sorting queue...');
+  const queue = jstor.get('activeQueue')
+    .map(i => i.id)
+    .concat(jstor.get('reviewQueue'));
+
+  queue.sort(make_comparator(ascending));
+
+  const active_queue_items = await fetch_review_items(queue.slice(0,10));
+  const active_queue = queue.slice(0,10).map(x => active_queue_items.find(i => i.id == x));
+  jstor.set('activeQueue', active_queue);
+  jstor.set('reviewQueue', queue.slice(10).reverse());
+  jstor.set('currentItem', active_queue[0]);
+
+  // this was inherited from previous script version -- no clue how nor why it
+  // works, but I did miss not having it around, so I'm bringing it back as an
+  // opt-in option
+  if (wkof.settings[script_settings_id].force1x1) {
     try {
       unsafeWindow.Math.random = function() { return 0; };
     } catch (e) {
@@ -116,33 +332,48 @@ function reorderBySrs(ascending = true) {
   }
 }
 
-/* Initialization */
+/* Counters */
+function register_counters() {
+  show_counters();
+  jstor.listenKeyChange('currentItem', show_counters);
+}
 
-$(function() {
-  const $spacer = $('<div style="background-color: #FFFFFF; color: #FFFFFF; cursor: pointer; display: inline-block; font-size: 0.8125em; padding: 2px; vertical-align: bottom;"/>');
-  const $ascButton = $('<div style="background-color: #A000f0; outline: 2px solid white; color: #FFFFFF; cursor: pointer; display: inline-block; font-size: 0.8125em; padding: 10px; vertical-align: bottom;">SRS Sort ↑</div>');
-  const $desButton = $('<div style="background-color: #A000f0; outline: 2px solid white; color: #FFFFFF; cursor: pointer; display: inline-block; font-size: 0.8125em; padding: 10px; vertical-align: bottom;">SRS Sort ↓</div>');
+function show_counters() {
+  const queue = jstor.get('activeQueue')
+    .map(i => i.id)
+    .concat(jstor.get('reviewQueue'));
 
-  $('footer').prepend($spacer);
-  $('footer').prepend($desButton.click(function() { reorderBySrs(false); }));
-  $('footer').prepend($ascButton.click(function() { reorderBySrs(true); }));
-
-  document.addEventListener('keydown', function(e) {
-    if (e.shiftKey && e.altKey && e.code == ascHotKey) {
-      reorderBySrs(true);
-      e.preventDefault();
-    } else if (e.shiftKey && e.altKey && e.code == desHotKey) {
-      reorderBySrs(false);
-      e.preventDefault();
-    }
+  const items_per_srs = [0, 0, 0, 0, 0, 0, 0, 0];
+  queue.forEach(function(i) {
+    ++items_per_srs[srs_stage_for(i)-1];
   });
 
-  if (sortOnStartup) {
-    const observer = new MutationObserver(function() {
-      reorderBySrs(true);
-      observer.disconnect();
-    });
-    observer.observe(document.getElementById('loading'), { attributes: true });
+  const $counters = $('<div id="srs-counters" style="background-color:rgba(255,255,255,0.9);border-radius:8px;color:black;font-weight:bold;margin-top:5px;text-shadow:none"></div>');
+  for (let srs = 1; srs <= items_per_srs.length; ++srs) {
+    const color = srs < 5 ? 'DD0093' : srs < 7 ? '882D9E' : srs < 8 ? '294DDB' : '0093DD';
+    if (srs > 1) {
+      $counters.append(', ');
+    }
+    $counters.append($('<span style="color:#' + color + ';margin:0">' + items_per_srs[srs - 1] + '</span>'));
   }
-});
+  $('#srs-counters').remove();
+  $('div#stats').append($counters);
+}
+
+/* Initialization */
+wkof.include('Menu,Settings,ItemData');
+wkof.ready('Menu,Settings,ItemData')
+  .then(install_menu)
+  .then(load_settings)
+  .then(load_assignments)
+  .then(register_hotkeys)
+  .then(register_counters)
+  .then(startup);
+
+function startup() {
+  if (wkof.settings[script_settings_id].sort_on_startup) {
+    sort_queue();
+  }
+  log('ready!');
+}
 // ==/UserScript==
